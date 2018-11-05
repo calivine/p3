7d34a6e1fe57d33c5ac7f9852417bc7728a922a9
Alex Caloggero <a.calog@protonmail.com>
2018-11-05 12:33:44 -0500

    Added display route; updated checkbox module

---
+++ resources/views/orders/home.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: calog
+ * Date: 11/5/2018
+ * Time: 10:42 AM
+ */
---
+++ resources/views/orders/product.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: calog
+ * Date: 11/5/2018
+ * Time: 12:32 PM
+ */
